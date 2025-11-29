044cb843279f5412f699c4073454b3b2f8dce16e
Mostafa Mahmoud <mostafapro87@gmail.com>
2025-11-29 15:56:34 +0200

    Bootstrapping Apps with Angular Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UsersComponent } from './users/users.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UsersComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
